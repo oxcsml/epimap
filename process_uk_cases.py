cd15b2be4d130fb2fe471c9019b8f748b2c7d12e
Ulrich Paquet <ulrich.paquet@gmail.com>
2020-07-14 17:23:49 +0100

    UTLA (Cornwall) fix

--- process_uk_cases.py
+++ process_uk_cases.py
@@ -1,10 +1,17 @@
-import pandas as pd
-import logging
-import sys
 from covid19_datasets import UKCovid19Data
 
 uk_data = UKCovid19Data()
 uk_cases = uk_data.get_cases_data()
 
-uk_cases.to_csv('uk_cases.csv')
+# Cases are combined for Cornwall and Isles of Scilly, but they exist as two
+# separate Upper Tier Local Authorities:
+# E06000052	Cornwall         (population 276,731)
+# E06000053	Isles of Scilly  (population 1,089)
+# We account both under 'Cornwall' for the purpose of creating a map.
 
+uk_cases.rename(
+    index={'Cornwall and Isles of Scilly': 'Cornwall'},
+    level='Area name',
+    inplace=True)
+
+uk_cases.to_csv('uk_cases.csv')

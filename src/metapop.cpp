8be5a2881b31398f9ce939644c63db85cdf1aebc
bryn <4146799+brynhayder@users.noreply.github.com>
2020-07-31 11:06:24 +0100

    keeping some temporary changes to metapop.cpp

--- src/metapop.cpp
+++ src/metapop.cpp
@@ -1,3 +1,4 @@
+#include <istream>
 #include <fstream>
 #include <iostream>
 #include <vector>
@@ -8,15 +9,16 @@
 #include "csv.h"
 #include "mobility.h"
 #include "xtensor/xarray.hpp"
+#include "xtensor/xcsv.hpp"
+#include "xtensor/xbuilder.hpp"
 
 using std::cout;
 using std::endl;
 using std::vector;
 using std::string;
+using std::ifstream;
 using std::ofstream;
-using std::valarray;
 
-void print_vector(vector<int> &vec);
 std::tuple<int, string> parse_cmd(int argc, char** argv);
 template <typename dtype> vector<vector<dtype>> valarrays_to_vector(const vector<valarray<dtype>> &arr);
 template <typename dtype> void write_csv(
@@ -27,33 +29,13 @@ const double attack = 1 / 5. ; // force of attack
 const double latent = 1 / 5.; // the mean of this variable is 1 / latent period
 const double inf_period = 1 / 14.; // length of period infected before recovering or dying
 
-/*
- * Integrate metapopulation modelling and put outputs to csv (maybe can use matrices for this?)
- * Read R_t in from a csv too (make python stuff to look at this)
- * Make some sort of visualisation so it looks like it's working
- * Figure out how to tune the R_t and find out what the right parameters for it should be (speak to bobby)
- * Speak to YW about the results
- *
- */
-// Later, use the seir initial values to calculate nppl
-// for now ca just put itin here since eventually will habe multiple populations anyway
-// can just remove the nppl argument because it's irrelevant!
-// 
-// --- Turn these populations into things that interact
-// --- find some way of testing it?
-// --- add in mobility data
-// --- add in time varying R_t
-// --- send it on to YW
-    
-//const string OUTPUT_FOLDER = "../outputs/";
-
-
-
 int main(int argc, char** argv) {
 
-    auto [timesteps, output_folder] = parse_cmd(argc, argv);
+    auto [timesteps, data_dir] = parse_cmd(argc, argv);
+    string output_folder = data_dir + "/outputs";
+    cout << "output folder is: " << output_folder << endl;
     
-    valarray<double> s, e, i, r, n;
+    xt::xarray<double> s, e, i, r, n;
     // need some way of initialising the values
     s = {980, 20, 9, 80, 880};
     e = {10, 880,  990, 900, 20};
@@ -61,28 +43,19 @@ int main(int argc, char** argv) {
     r = {0, 0, 0, 0, 0};
     n = s + e + i + r;
 
-    vector<valarray<double>> susceptible = {s};
-    vector<valarray<double>> exposed = {e};
-    vector<valarray<double>> infected = {i};
-    vector<valarray<double>> recovered = {r};
-    vector<valarray<double>> population = {n};
-    
-    valarray<double> ones = s;
-    std::fill(begin(ones), end(ones), 1);
+    tuple<xt::xarray<double>> susceptible = {s};
+    tuple<xt::xarray<double>> exposed = {e};
+    tuple<xt::xarray<double>> infected = {i};
+    tuple<xt::xarray<double>> recovered = {r};
+    tuple<xt::xarray<double>> population = {n};
 
-    vector<valarray<double>> mob_data = {
-        ones,
-        ones,
-        ones,
-        ones,
-        ones
-    };
-
-    MobilityMatrix<double> mobility(mob_data);
+    ifstream file;
+    file.open(data_dir + "/mobility.csv");
+    auto mobility =  xt::load_csv<double>(file);
 
     for (int t=0; t != timesteps; ++t) {
-        valarray<double> sn = s / n;
-        s = s - attack * i * s / n + mobility.vecmulr(sn);
+        //xt::xarray<double> sn = s / n;
+        s = s - attack * i * s / n;
         e = e + attack * i * s / n - latent * e;
         i = i + latent * e - inf_period * i;
         r = r + inf_period * i;
@@ -111,7 +84,7 @@ int main(int argc, char** argv) {
 
 std::tuple<int, string> parse_cmd(int argc, char** argv) {
     int timesteps;
-    string output_folder;
+    string data_dir;
     
     if (argc < 3) {
         throw std::domain_error(
@@ -119,23 +92,31 @@ std::tuple<int, string> parse_cmd(int argc, char** argv) {
                 );
     } else {
        timesteps = std::stoi(argv[1]);
-       output_folder = argv[2];
+       data_dir = argv[2];
     } 
 
-    std::tuple<int, string> tup(timesteps, output_folder);
+    std::tuple<int, string> tup(timesteps, data_dir);
     return tup;
 } 
 
-template <typename dtype>
-vector<vector<dtype>> valarrays_to_vector(const vector<valarray<dtype>> &arr) {
-    vector<vector<dtype>> outvec;
-    for (const auto &vec : arr) {
-        vector<dtype> newvec;
-        std::copy(begin(vec), end(vec), std::back_inserter(newvec));
-        outvec.push_back(newvec);
-    } 
-    return outvec;
-} 
+/*
+ * Integrate metapopulation modelling and put outputs to csv (maybe can use matrices for this?)
+ * Read R_t in from a csv too (make python stuff to look at this)
+ * Make some sort of visualisation so it looks like it's working
+ * Figure out how to tune the R_t and find out what the right parameters for it should be (speak to bobby)
+ * Speak to YW about the results
+ *
+ */
+// Later, use the seir initial values to calculate nppl
+// for now ca just put itin here since eventually will habe multiple populations anyway
+// can just remove the nppl argument because it's irrelevant!
+// 
+// --- Turn these populations into things that interact
+// --- find some way of testing it?
+// --- add in mobility data
+// --- add in time varying R_t
+// --- send it on to YW
+    
 
 template <typename dtype>
 void write_csv(const char* fname, const vector<vector<dtype>> &data, const vector<string> &names){

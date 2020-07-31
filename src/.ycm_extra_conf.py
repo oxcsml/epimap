af8d26761c0c547ddb376ca145aff46cf1cbb2f6
bryn <4146799+brynhayder@users.noreply.github.com>
2020-07-31 10:49:14 +0100

    compilation updates

--- src/.ycm_extra_conf.py
+++ src/.ycm_extra_conf.py
@@ -1,2 +1,21 @@
+import os
+
+
+SOURCE_EXTENSIONS = [ '.cpp', '.cxx', '.cc', '.h', '.hpp']
+
+
 def Settings(**kwargs):
-    return {'flags': ['-Wall', '-std=c++17']}
+    environ = os.environ["CONDA_PREFIX"]
+    return {
+        "flags": [
+            "-x",
+            "c++",
+            "-Wall",
+            "-Wextra",
+            "-Wpedantic",
+            "-std=c++17",
+            "-isystem", f"{environ}",
+            "-I",
+            f"{environ}/include",
+        ]
+    }

444c41d57c9809616d53ce9df88984904c26a15c
bryn <4146799+brynhayder@users.noreply.github.com>
2020-08-03 16:35:52 +0100

    changed file saving to integer format to help loading

--- src/make_data.py
+++ src/make_data.py
@@ -10,10 +10,13 @@ if __name__ == "__main__":
     parser.add_argument("--delimiter", type=str, help="csv delimiter", default=",")
     args = parser.parse_args()
 
+    def save_csv(fname, arr):
+        return np.savetxt(
+            os.path.join(args.folder, fname), arr, delimiter=args.delimiter, fmt="%d",
+        )
+
     mobility = np.ones((5, 5))
-    np.savetxt(
-        os.path.join(args.folder, "mobility.csv"), mobility, delimiter=args.delimiter
-    )
+    save_csv("mobility.csv", mobility)
 
     # initial values
     s = [980, 20, 9, 80, 880]
@@ -21,6 +24,12 @@ if __name__ == "__main__":
     i = [10, 100, 1, 20, 100]
     r = [0, 0, 0, 0, 0]
     init = np.array([s, e, i, r])
-    np.savetxt(
-        os.path.join(args.folder, "initial-values.csv"), init, delimiter=args.delimiter
-    )
+    save_csv("initial-values.csv", init)
+
+    # seed epidemic on one place
+    s = [400, 1000, 1000, 1000, 1000]
+    e = [500, 0, 0, 0, 0]
+    i = [100, 0, 0, 0, 0]
+    r = [0, 0, 0, 0, 0]
+    init_seed = np.array([s, e, i, r])
+    save_csv("initial-values-seed.csv", init_seed)

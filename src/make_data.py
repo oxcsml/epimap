19989ba017869af09e0484cc1e7beec54dd20138
bryn <4146799+brynhayder@users.noreply.github.com>
2020-08-05 10:52:38 +0100

    added more examples to data making process. Added replicatino of gostic et al setup

--- src/make_data.py
+++ src/make_data.py
@@ -1,35 +1,144 @@
 from argparse import ArgumentParser
+from functools import wraps
 import os
 
 import numpy as np
+import pandas as pd
 
 
 if __name__ == "__main__":
+    __register = list()
+
     parser = ArgumentParser()
     parser.add_argument("folder", type=str, help="Where to save the data")
     parser.add_argument("--delimiter", type=str, help="csv delimiter", default=",")
     args = parser.parse_args()
 
-    def save_csv(fname, arr):
-        return np.savetxt(
-            os.path.join(args.folder, fname), arr, delimiter=args.delimiter, fmt="%d",
+    def save_csv(fname, df, fmt, header=True, index=False):
+        return df.to_csv(
+            os.path.join(args.folder, fname),
+            sep=args.delimiter,
+            float_format=fmt,
+            header=header,
+            index=index,
         )
 
-    mobility = np.ones((5, 5))
-    save_csv("mobility.csv", mobility)
+    def save(exp_name):
+        def wrapper(func):
+            @wraps(func)
+            def inner(*args, **kwargs):
+                """This enforces saving the cols and index in consistent order"""
+                mobility, initial_values, rt = func(*args, **kwargs)
+                region_names = initial_values.columns
+                save_csv(
+                    f"{exp_name}-mobility.csv",
+                    mobility[region_names].loc[region_names],
+                    fmt="%d",
+                    header=False,
+                )
+                save_csv(
+                    f"{exp_name}-initial-values.csv",
+                    initial_values[region_names],
+                    fmt="%d",
+                    header=False,
+                )
+                save_csv(
+                    f"{exp_name}-rt.csv", rt[region_names], fmt="%.5f", header=False
+                )
+                return
 
-    # initial values
-    s = [980, 20, 9, 80, 880]
-    e = [10, 880, 990, 900, 20]
-    i = [10, 100, 1, 20, 100]
-    r = [0, 0, 0, 0, 0]
-    init = np.array([s, e, i, r])
-    save_csv("initial-values.csv", init)
+            __register.append(inner)
+            return inner
 
-    # seed epidemic on one place
-    s = [400, 1000, 1000, 1000, 1000]
-    e = [500, 0, 0, 0, 0]
-    i = [100, 0, 0, 0, 0]
-    r = [0, 0, 0, 0, 0]
-    init_seed = np.array([s, e, i, r])
-    save_csv("initial-values-seed.csv", init_seed)
+        return wrapper
+
+    @save(exp_name="basic")
+    def basic_example():
+        region_names = "A B C D E".split()
+        mobility = pd.DataFrame(
+            np.ones((5, 5)), index=region_names, columns=region_names
+        )
+        s = [400, 1000, 1000, 1000, 1000]
+        e = [500, 0, 0, 0, 0]
+        i = [100, 0, 0, 0, 0]
+        r = [0, 0, 0, 0, 0]
+        initial_values = pd.DataFrame([s, e, i, r], columns=region_names)
+        rt = pd.DataFrame(np.full((1000, 5), 2.8), columns=region_names)
+        return mobility, initial_values, rt
+
+    @save(exp_name="iid")
+    def iid():
+        region_names = "A B C D E".split()
+        mobility = pd.DataFrame(
+            np.zeros((5, 5)), index=region_names, columns=region_names
+        )
+        s = [800, 800, 800, 800, 800]
+        e = [100, 100, 100, 100, 100]
+        i = [100, 100, 100, 100, 100]
+        r = [0, 0, 0, 0, 0]
+        initial_values = pd.DataFrame([s, e, i, r], columns=region_names)
+        rt = pd.DataFrame(np.full((10000, 5), 2.8), columns=region_names)
+        return mobility, initial_values, rt
+
+    @save(exp_name="identical-uniform-movement")
+    def identical_uniform_movement():
+        region_names = "A B C D E".split()
+        mobility = pd.DataFrame(
+            np.ones((5, 5)), index=region_names, columns=region_names
+        )
+        s = [800, 800, 800, 800, 800]
+        e = [100, 100, 100, 100, 100]
+        i = [100, 100, 100, 100, 100]
+        r = [0, 0, 0, 0, 0]
+        initial_values = pd.DataFrame([s, e, i, r], columns=region_names)
+        rt = pd.DataFrame(np.full((10000, 5), 2.8), columns=region_names)
+        return mobility, initial_values, rt
+
+    @save(exp_name="single")
+    def single_region():
+        region_names = ["A"]
+        mobility = pd.DataFrame(
+            np.ones((1, 1)), index=region_names, columns=region_names
+        )
+        s = [800]
+        e = [100]
+        i = [100]
+        r = [0]
+        initial_values = pd.DataFrame([s, e, i, r], columns=region_names)
+        rt = pd.DataFrame(np.full((10000, 1), 2.8), columns=region_names)
+        return mobility, initial_values, rt
+
+    @save(exp_name="gostic-single")
+    def gostic_single_region():
+        """
+        In the gostic paper it seems that they have some continuous decrease in 
+        R, rather than these abrupt changes --- is this due to fewer people
+        being susceptible?
+
+        There is a comment in their code simulation.R that describes a variable called 
+        CONTINUOUS, but I can't find it being used :s
+        """
+        region_names = ["A"]
+        mobility = pd.DataFrame(
+            np.ones((1, 1)), index=region_names, columns=region_names
+        )
+        s = [2e6 - 60]
+        e = [0]
+        i = [60]
+        r = [0]
+        initial_values = pd.DataFrame([s, e, i, r], columns=region_names)
+        rt = pd.DataFrame(
+            np.interp(
+                np.arange(300),
+                [0, 60, 67, 90, 97, 300],
+                [2.0, 2.0, 0.8, 0.8, 1.15, 1.15],
+            ),
+            columns=region_names
+        )
+        return mobility, initial_values, rt
+
+    [f() for f in __register]
+
+    # gostic single
+    # yw town and village thing
+    # put in sensible parameters for stuff from reading paper

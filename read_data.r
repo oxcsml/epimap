e881249d65e64c8be9b3653fa873d0c1007e6846
ywteh <yeewhye@gmail.com>
2020-08-13 21:51:36 +0100

    process_data.r and process_radiation_fluxes.r

--- read_data.r
+++ read_data.r
@@ -11,17 +11,6 @@ population <- df[,3]
 geodist <- read.csv("data/distances.csv",row.names=1)
 colnames(geodist) <- areas
 
-radiation_length_scales <- c(.1,.2,.5)
-radiation_flux <- array(0,dim=c(N,N,length(radiation_length_scales)))
-for (i in 1:length(radiation_length_scales)) {
-  ls <- radiation_length_scales[i]
-  df <- data.matrix(read.csv(sprintf('data/radiation_flux_ls=%1.1f.csv',ls),row.names=1))
-  radiation_flux[,,i] <- df
-}
-colnames(radiation_flux) <- areas
-rownames(radiation_flux) <- areas
-dimnames(radiation_flux)[[3]] <- radiation_length_scales
-
 # Use counts from uk_cases in case updated
 
 uk_cases <- read.csv("data/uk_cases.csv")

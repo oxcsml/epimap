d90faafb811f41e24c9950c3855c8402772bf756
ywteh <yeewhye@gmail.com>
2020-08-19 16:11:20 +0100

    small edits run-time-vary

--- run-time-vary.r
+++ run-time-vary.r
@@ -9,8 +9,8 @@ option_list = list(
   make_option(c("-m", "--metapop"),       type="character", default="radiation2_uniform_in",help="metapopulation model for inter-region cross infections (uniform_in{_out}/[radiation{1[2]3}_uniform_in{_out}]/none)"),
   make_option(c("-o", "--observation"),   type="character", default="negative_binomial_3",  help="observation model ([negative_binomial_{2[3]}]/poisson)"),
   make_option(c("-c", "--chains"),        type="integer",   default=4,                      help="number of MCMC chains [4]"),
-  make_option(c("-i", "--iterations"),    type="integer",   default=6000,                   help="Length of MCMC chains [4000]"),
-  make_option(c("-n", "--time_steps"),    type="integer",   default=6,                      help="Number of periods to fit Rt in"),
+  make_option(c("-i", "--iterations"),    type="integer",   default=6000,                   help="Length of MCMC chains [6000]"),
+  make_option(c("-n", "--time_steps"),    type="integer",   default=12,                      help="Number of periods to fit Rt in"),
   make_option(c("-t", "--task_id"),       type="integer",   default=0,                      help="Task ID for Slurm usage. By default, turned off [0].")
 ); 
 
@@ -165,13 +165,15 @@ Rmap_data <- list(
   # gp_length_scale_sd = opt$gp_length_scale_sd
 )
 
-runname = sprintf('Rmap-time-vary-%s-%s-%s-%s-%s-%s', 
+runname = sprintf('Rmap-time-vary-%s-%s-%s-%s-%s-%s-%s', 
+  as.character(Sys.time(),format='%Y%m%d%H%M%S'),
   opt$spatialkernel, 
   opt$localkernel, 
   opt$globalkernel, 
   opt$metapop, 
   opt$observation,
-  opt$time_steps)
+  opt$time_steps
+)
 print(runname)
 
 
@@ -205,7 +207,7 @@ saveRDS(fit, paste('fits/', runname, '_stanfit', '.rds', sep=''))
 # fit = readRDS(paste('fits/', runname, '_stanfit', '.rds', sep=''))
 
 print(summary(fit, 
-    pars=c("R0","gp_space_length_scale","gp_space_sigma","gp_time_length_scale","global_sigma","precision","coupling_rate"), 
+    pars=c("R0","gp_space_length_scale","gp_space_sigma","gp_time_length_scale","global_sigma","local_scale","precision","coupling_rate"), 
     probs=c(0.025, 0.25, 0.5, 0.75, 0.975))$summary)
 
 

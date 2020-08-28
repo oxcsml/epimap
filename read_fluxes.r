803ea3ca75924e14a5cf75aee1b2a5c6c93b4dd8
Bobby He <bhe@zizgpu01.cpu.stats.ox.ac.uk>
2020-08-29 00:42:22 +0100

    add traffic run.r

--- read_fluxes.r
+++ read_fluxes.r
@@ -10,4 +10,8 @@ colnames(radiation_flux) <- areas
 rownames(radiation_flux) <- areas
 dimnames(radiation_flux)[[3]] <- radiation_length_scales
 
-
+traffic_flux <- array(0, dim=c(N,N,1))
+df <- data.matrix(read.csv('data/traffic_flux.csv', row.names=1))
+traffic_flux[,,1] <- df
+colnames(traffic_flux) <- areas
+rownames(traffic_flux) <- areas

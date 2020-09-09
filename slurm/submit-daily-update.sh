e8254615339e56e69b13ade261d80b93be40fc79
Michael Hutchinson <mhutchin@ziz.stats.ox.ac.uk>
2020-09-09 15:39:04 +0100

    Daily update scripts

--- slurm/submit-daily-update.sh
+++ slurm/submit-daily-update.sh
@@ -8,37 +8,13 @@
 #SBATCH --partition=ziz-large
 #
 #SBATCH --ntasks=1
-#SBATCH --mem-per-cpu=15G # `seff' indicated that we need between 1.5-2G mem-per-cpu. 
+#SBATCH --mem-per-cpu=20G # `seff' indicated that we need between 1.5-2G mem-per-cpu. 
 #SBATCH --cpus-per-task=7
 #
 #SBATCH --array=1
 
 set -e
 
-git pull
-
-conda activap Rmap
-
-make preprocess-data
-
-output_file="Rmap-time-vary-reduce-latest-matern12-local-global-radiation2_uniform_in-negative_binomial_3"
-time_steps=15
-
-Rscript run-time-vary-reduce.r --time_steps=$time_steps --iterations 8000 --chains 6 --daily_update
-
-python3 reinflate.py \
-    fits/latest_updates/${output_file}-${time_steps}_Rt.csv \
-    fits/latest_updates/${output_file}-${time_steps}_Pexceed.csv \
-    fits/latest_updates/${output_file}-${time_steps}_Cpred.csv \
-    fits/latest_updates/${output_file}-${time_steps}_Cproj.csv \
-    docs/assets/data/generated/Rt.csv \
-    docs/assets/data/generated/Pexceed.csv \
-    docs/assets/data/generated/Cpred.csv \
-    docs/assets/data/generated/Cproj.csv
-
-git add docs/assets/data/generated/*
-git commit -m "daily update"
-git push
-
+Rscript run-time-vary-reduce.r --time_steps=15 --iterations 8000 --chains 6 --daily_update
 
 echo 'Run completed.'

57293138b64ece62a537083a45f2ccb4216b4ca6
bryn <4146799+brynhayder@users.noreply.github.com>
2020-09-10 10:28:09 +0100

    Revert "think michael uses this file and not needed here, right?"
    This reverts commit 3913cbdd074db3119e591d6543516432d8a8da02.

---
+++ docs/process_site_data.py
@@ -0,0 +1,27 @@
+"""Pre-process data for the visualisation website."""
+
+import pandas as pd
+import logging
+import sys
+
+UK_CASES_PATH = '../data/uk_cases.csv'
+OUTPUT_PATH = 'site_data.csv'
+# RtCproj_PATH = '../data/RtCproj.csv'
+
+uk_cases = pd.read_csv(UK_CASES_PATH)
+# RtCproj = pd.read_csv(RtCproj_PATH)
+
+df = uk_cases.set_index(['Country', 'Area name']).stack().to_frame().reset_index().rename(columns={
+    'Area name': 'area',
+    'level_2': 'Date',
+    0: 'cases_new'
+})
+df['cases_new_smoothed'] = df['cases_new'].rolling(7, center=True).mean()
+df['Date'] = pd.to_datetime(df['Date'])
+
+# Remove the last 5 days of actual cases which are exluded in the modelling due to being unreliable
+max_date = df['Date'].max()
+df = df[df['Date'] < max_date - pd.offsets.Day(5)]
+
+df.to_csv(OUTPUT_PATH, index=False)
+print('Wrote', OUTPUT_PATH)

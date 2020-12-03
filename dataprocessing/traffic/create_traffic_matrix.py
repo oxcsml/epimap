0cfab06ca3fcb3a8b454406e961b0f828f63ace0
Yee Whye Teh <teh@ziz.stats.ox.ac.uk>
2020-12-03 00:18:54 +0000

    move traffic/traffic to data/uk_traffic

--- dataprocessing/traffic/create_traffic_matrix.py
+++ dataprocessing/traffic/create_traffic_matrix.py
@@ -87,4 +87,4 @@ if __name__ == "__main__":
             if k % 1000 == 0:
                 print(f"Done {k}/{NN} entries.")
 
-    traffic.to_csv("traffic/traffic.csv")
+    traffic.to_csv("data/uk_traffic.csv")

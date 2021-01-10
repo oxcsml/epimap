ab3ff7cfe01525828a8a4b45883e840ce964ad4f
Michael Hutchinson <hutchinson.micahel.john@gmail.com>
2021-01-10 15:03:50 +0000

    Small bugfix

--- dataprocessing/traffic/process_alt_traffic.py
+++ dataprocessing/traffic/process_alt_traffic.py
@@ -9,7 +9,7 @@ areas = pd.read_csv("data/areas.csv", index_col=0)
 assert (np.diag(traffic) > 0).all()
 assert (traffic.sum(axis=1) > 0).all()
 
-# %% 
+# %%
 
 night_populations = areas["population"]
 commuting_populations = traffic.sum(axis=1)
@@ -17,16 +17,14 @@ noncommuting_populations = night_populations - commuting_populations
 
 # forward flow
 forward_flow = traffic + np.diag(noncommuting_populations)
+populations = forward_flow.sum(axis=1)
 forward_flow = forward_flow.divide(populations, axis="index")
 assert np.isclose(forward_flow.sum(axis=1).values, 1).all()
 forward_flow.to_csv("data/uk_forward_commute_flow.csv")
 
 # reverse flow
-reverse_flow = traffic.T + np.diag(noncommuting_populations) 
+reverse_flow = traffic.T + np.diag(noncommuting_populations)
 day_populations = reverse_flow.sum(axis=1)
 reverse_flow = reverse_flow.divide(day_populations, axis="index")
 assert np.isclose(reverse_flow.sum(axis=1).values, 1).all()
 reverse_flow.to_csv("data/uk_reverse_commute_flow.csv")
-
-
-

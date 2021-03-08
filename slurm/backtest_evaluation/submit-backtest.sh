10974a0d391db6b0566769f0a19523883a251b80
Sheheryar Zaidi <sheh_zaidi96@hotmail.com>
2021-03-08 12:58:13 +0000

    added new script to launch backtests

--- slurm/backtest_evaluation/submit-backtest.sh
+++ slurm/backtest_evaluation/submit-backtest.sh
@@ -2,12 +2,12 @@
 
 set -e
 
-function clean_and_map { # cleaning_directory = $1, options_clean = $2, weeks_modelled = $3, results_directory = $4, options_map = $5
+function clean_and_map { # options_clean = $1, first_day_modelled = $2, results_directory = $3, options_map = $4
     sbatch --wait \
         --mail-user=$USER@stats.ox.ac.uk \
         --mail-type=ALL \
-        --job-name=clean_ts_weeks_"$3" \
-        --output="$1"/output/clean_%A_%a.out \
+        --job-name=Rmap_singlearea_start_"$2" \
+        --output="$3"/singlearea/output/run_%A_%a.out \
         --partition=ziz-medium \
         --ntasks=1 \
         --time=18:00:00 \
@@ -15,101 +15,130 @@ function clean_and_map { # cleaning_directory = $1, options_clean = $2, weeks_mo
         --cpus-per-task=1 \
         --array=1-348 \
         --wrap \
-        "Rscript cleaning/clean_area.r --task_id \$SLURM_ARRAY_TASK_ID $2 && echo clean_area: DONE"
+        "Rscript covidmap/stage1_run.r --area_index \$SLURM_ARRAY_TASK_ID $1 && echo clean_area: DONE"
     wait # are these waits needed?
 
-    echo "submit-clean: Combining areas ($3 weeks)"
+    echo "submit-clean: Combining areas (start: $2)"
 
     sbatch --wait \
         --mail-user=$USER@stats.ox.ac.uk \
         --mail-type=ALL \
-        --job-name=combine_areas_weeks_"$3" \
-        --output="$1"/output/combine_%A_%a.out \
+        --job-name=Rmap_combineareas_start_"$2" \
+        --output="$3"/singlearea/output/combine_%A_%a.out \
         --partition=ziz-medium \
         --ntasks=1 \
         --time=18:00:00 \
         --mem-per-cpu=10G \
         --cpus-per-task=1 \
         --wrap \
-        "Rscript cleaning/combine_areas.r $2 && echo combine_areas: DONE"
+        "Rscript covidmap/stage1_combine.r $1 && echo combine_areas: DONE"
     wait
 
-    echo "submit-clean: DONE ($3 weeks)"
+    echo "submit-clean: DONE (start: $2)"
 
     sbatch --wait \
         --mail-user=$USER@stats.ox.ac.uk \
         --mail-type=ALL \
-        --job-name=Rmap_run_weeks_"$3" \
-        --output="$4"/output/run_%A_%a.out \
+        --job-name=Rmap_regional_start_"$2" \
+        --output="$3"/regional/output/run_%A_%a.out \
         --partition=ziz-large \
         --ntasks=1 \
         --cpus-per-task=1 \
-        --mem-per-cpu=20G \
-        --array=1-10 \
+        --mem-per-cpu=10G \
+        --array=1-9 \
         --wrap \
-        "Rscript mapping/run.r $5 \
-            --cleaned_sample_id \$SLURM_ARRAY_TASK_ID \
+        "Rscript covidmap/stage2_run.r $4 \
+            --region_id \$SLURM_ARRAY_TASK_ID \
             && echo run: DONE"
 
-    echo submit-run: Merging results
+    echo "submit-run: Merging results"
 
     sbatch --wait \
         --mail-user=$USER@stats.ox.ac.uk \
         --mail-type=ALL \
-        --job-name=Rmap_merge_weeks_"$3" \
-        --output="$4"/output/merge_%A_%a.out \
+        --job-name=Rmap_mergeregions_start_"$2" \
+        --output="$3"/regional/output/merge_%A_%a.out \
         --partition=ziz-large \
         --ntasks=1 \
         --cpus-per-task=1 \
-        --mem-per-cpu=100G \
+        --mem-per-cpu=10G \
         --wrap \
-        "Rscript mapping/merge_results.r $5 && echo merge: DONE"
+        "Rscript covidmap/stage2_merge.r $4 && echo merge: DONE"
 
     wait
     
-    echo submit-run: DONE
+    echo "submit-run: DONE"
 
 }
 
-# TODO: turn these into positional args later?
-first_day_modelled="2020-06-01"
-weeks_modelled_array=(4 6 8 10 12 14 16)
-backtest_directory="fits/backtests"
+# TODO: turn these into positional args for this script later?
+backtest_directory="fits/backtests" # relative to Rmap working directory!
+
+first_day_modelled_array=("2020-08-24" "2020-09-07" "2020-09-21" "2020-10-05" "2020-10-19") 
+weeks_modelled=15
+
+space_scales=("0" "0.01" "0.05" "0.1" "0.2" "0.5" "1.0")
 
 echo compiling
-Rscript mapping/compile.r
+# Rscript epimap/compile.r
 
-for w in "${weeks_modelled_array[@]}"
+for space_scale in "${space_scales[@]}"
 do
-    clean_directory="${backtest_directory}/clean_start_${first_day_modelled}_weeks_${w}"
-    results_directory="${backtest_directory}/map_start_${first_day_modelled}_weeks_${w}"
-    mkdir -p "$clean_directory"
-    mkdir -p "$clean_directory"/pdfs
-    mkdir -p "$clean_directory"/stanfits
-    mkdir -p "$clean_directory"/output
+    # space_scale=${space_scales[$model_idx]}
+    for first_day_modelled in "${first_day_modelled_array[@]}"
+    do
+        results_directory="${backtest_directory}/space_${space_scale}/start_${first_day_modelled}_weeks_${weeks_modelled}"
+        mkdir -p "$results_directory"
+        mkdir -p "$results_directory"/singlearea
+        mkdir -p "$results_directory"/singlearea/pdfs
+        mkdir -p "$results_directory"/singlearea/stanfits
+        mkdir -p "$results_directory"/singlearea/output
 
-    mkdir -p "$results_directory"/output
+        mkdir -p "$results_directory"/regional
+        mkdir -p "$results_directory"/regional/output
 
-    options_clean="\
-        --clean_directory $clean_directory \
-        --first_day_modelled $first_day_modelled \
-        --weeks_modelled $w \
-    "
+        cp data/cases.csv $results_directory
+        
+        options_clean="\
+            --results_directory $results_directory \
+            --produce_plots TRUE \
+            --first_day_modelled $first_day_modelled \
+            --weeks_modelled $weeks_modelled \
+        "
 
-    # --time_steps 15 \ no longer needed?
+        if [ $space_scale == 0 ]
+        then
+            options_map="\
+                --approximation regional \
+                --globalkernel none \
+                --spatialkernel none \
+                --results_directory $results_directory \
+                --fixed_gp_time_length_scale 100.0 \
+                --first_day_modelled $first_day_modelled \
+                --weeks_modelled $weeks_modelled \
+            "
+        else
+            options_map="\
+                --approximation regional \
+                --globalkernel none \
+                --spatialkernel matern12 \
+                --results_directory $results_directory \
+                --fixed_gp_space_length_scale $space_scale \
+                --fixed_gp_time_length_scale 100.0 \
+                --first_day_modelled $first_day_modelled \
+                --weeks_modelled $weeks_modelled \
+            "
+        fi
 
-    options_map="\
-        --iterations 6000 \
-        --observation_data cleaned_recon_sample \
-        --observation_model negative_binomial_3 \
-        --clean_directory $clean_directory \
-        --results_directory $results_directory \
-        --first_day_modelled $first_day_modelled \
-        --weeks_modelled $w \
-    "
+        clean_and_map "$options_clean" "$weeks_modelled" "$results_directory" "$options_map" &
 
-    clean_and_map "$clean_directory" "$options_clean" "$w" "$results_directory" "$options_map" &
+    done
 
+    wait
+
+    # get rid of all space consuming rds files.
+    rm -rf /data/ziz/not-backed-up/scratch/szaidi/Rmap/${backtest_directory}/*/*/regional/*.rds
+    rm -rf /data/ziz/not-backed-up/scratch/szaidi/Rmap/${backtest_directory}/*/*/singlearea/stanfits/*.rds
+
+    echo "Completed a model config backtest."
 done
-
-wait

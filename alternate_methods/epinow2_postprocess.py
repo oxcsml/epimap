173a41d98e7f356ae9df0797137b35f6a76b694d
Bryn Elesedy <4146799+brynhayder@users.noreply.github.com>
2021-04-16 14:40:36 +0100

    (hopefully) fix labelling issue

--- alternate_methods/epinow2_postprocess.py
+++ alternate_methods/epinow2_postprocess.py
@@ -45,14 +45,15 @@ def make_dfs(
         samples = np.loadtxt(fpath)
 
         days_modelled = samples.shape[1]
+        start_d = pd.Timestamp(start_date)
         end_date = (
-            pd.Timestamp(start_date)
+            start_d
             + pd.Timedelta(weeks_modelled, unit="W")
             + pd.Timedelta(forecast_days, unit="D")
         )
         dates = pd.date_range(
+            start=start_d,
             end=end_date,
-            periods=days_modelled,
             freq="D"
         )
         provenance = np.r_[

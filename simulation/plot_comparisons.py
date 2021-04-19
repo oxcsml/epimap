c8a008baea074bc24e25da5665a696059916087a
Michael Hutchinson <hutchinson.micahel.john@gmail.com>
2021-04-19 12:04:04 +0100

    plotting update

--- simulation/plot_comparisons.py
+++ simulation/plot_comparisons.py
@@ -2,10 +2,13 @@
 import os
 import math
 from itertools import chain
+from collections import defaultdict
 
 import numpy as np
 import pandas as pd
 import matplotlib
+import matplotlib.dates as mdates
+from matplotlib.ticker import MaxNLocator
 
 matplotlib.use("Agg")
 import matplotlib.pyplot as plt
@@ -44,10 +47,15 @@ def load_all(results_dir):
 
     df = load_parameter("merged_Rt" if merged else "Rt", results_dir)
 
-    cpred = load_parameter("merged_Cpred" if merged else "Cpred", results_dir)
-    cproj = load_parameter("merged_Cproj" if merged else "Cproj", results_dir)
-    c = pd.concat([cproj, cpred])
-    df = pd.merge(df, c, on=["area", "Date", "provenance"])
+    # cpred = load_parameter("merged_Cpred" if merged else "Cpred", results_dir)
+    # cproj = load_parameter("merged_Cproj" if merged else "Cproj", results_dir)
+    # c = pd.concat([cproj, cpred])
+    # df = pd.merge(df, c, on=["area", "Date", "provenance"])
+
+    bpred = load_parameter("merged_Bpred" if merged else "Bpred", results_dir)
+    bproj = load_parameter("merged_Bproj" if merged else "Bproj", results_dir)
+    b = pd.concat([bproj, bpred])
+    df = pd.merge(df, b, on=["area", "Date", "provenance"])
 
     # xpred = load_parameter("merged_Xpred" if merged else "Xpred", results_dir)
     # xproj = load_parameter("merged_Xproj" if merged else "Xproj", results_dir)
@@ -56,6 +64,9 @@ def load_all(results_dir):
 
     # pexceed = load_parameter("merged_Pexceed" if merged else "Pexceed", results_dir)
     # df = pd.merge(df, pexceed, on=["area", "Date", "provenance"])
+
+    data_cols = df.columns.drop(["area", "Date", "provenance"])
+    df[data_cols] = df[data_cols].replace({"  NA": "NaN"}).astype(np.float32)
 
     return df
 
@@ -75,8 +86,10 @@ def plot_estimate(
     par_name,
     provenance=None,
     median="_50",
-    upper="_97_5",
-    lower="_2_5",
+    upper1="_97_5",
+    lower1="_2_5",
+    upper2="_97_5",
+    lower2="_2_5",
     color=None,
     ax=None,
 ):
@@ -91,7 +104,7 @@ def plot_estimate(
     else:
         linestyle = "solid"
 
-    if color == None:
+    if color is None:
         line = ax.plot(
             results.Date,
             results[par_name + median],
@@ -100,17 +113,21 @@ def plot_estimate(
             zorder=1,
         )
         color = (line[0].get_color(),)
-        fill = ax.fill_between(
+        fill1 = ax.fill_between(
             results.Date,
-            results[par_name + lower],
-            results[par_name + upper],
+            results[par_name + lower1],
+            results[par_name + upper1],
             alpha=0.2 if provenance == "projected" else 0.4,
             color=color,
             zorder=2,
-            # hatch="///" if provenance == "projected" else None,
-            # facecolor=lighten_color(color, 0.5),
-            # edgecolor=lighten_color(color, 1.6),
-            # linewidth=0.0,
+        )
+        fill2 = ax.fill_between(
+            results.Date,
+            results[par_name + lower2],
+            results[par_name + upper2],
+            alpha=0.2 if provenance == "projected" else 0.4,
+            color=color,
+            zorder=2,
         )
     else:
         line = ax.plot(
@@ -121,10 +138,22 @@ def plot_estimate(
             linestyle=linestyle,
             zorder=1,
         )
-        fill = ax.fill_between(
+        fill1 = ax.fill_between(
             results.Date,
-            results[par_name + lower],
-            results[par_name + upper],
+            results[par_name + lower1],
+            results[par_name + upper1],
+            alpha=0.4 if provenance == "projected" else 0.4,
+            color=color,
+            zorder=2,
+            # hatch="///" if provenance == "projected" else None,
+            # facecolor=lighten_color(color, 0.5),
+            # edgecolor=lighten_color(color, 1.6),
+            # linewidth=0.0,
+        )
+        fill2 = ax.fill_between(
+            results.Date,
+            results[par_name + lower2],
+            results[par_name + upper2],
             alpha=0.2 if provenance == "projected" else 0.4,
             color=color,
             zorder=2,
@@ -136,15 +165,26 @@ def plot_estimate(
 
     ax.tick_params(axis="x", rotation=45)
 
-    return (line, fill), ax
+    return (line, fill1, fill2), ax
 
 
-def plot_actual(results, par_name, color=None, ax=None):
+def plot_actual(results, par_name, color=None, scatter=False, ax=None):
     if ax is None:
         fig, ax = plt.subfigures(1, 1)
 
-    if color == None:
-        ax.plot(results.Date, results[par_name], zorder=1)
+    # if color == None:
+    #     # ax.plot(results.Date, results[par_name], zorder=1)
+    #     ax.scatter(results.Date, results[par_name], zorder=1)
+    # else:
+    if scatter:
+        ax.scatter(
+            results.Date,
+            results[par_name],
+            marker="+",
+            s=4,
+            color=color,
+            zorder=1,
+        )
     else:
         ax.plot(results.Date, results[par_name], color=color, zorder=1)
 
@@ -155,13 +195,13 @@ def plot_actual(results, par_name, color=None, ax=None):
 
 def plot_estimate_area(results, area, *args, **kwargs):
     handles, ax = plot_estimate(results[results.area == area], *args, **kwargs)
-    ax.set_title(area)
+    # ax.set_title(area)
     return handles, ax
 
 
 def plot_actual_area(results, area, *args, **kwargs):
     ax = plot_actual(results[results.area == area], *args, **kwargs)
-    ax.set_title(area)
+    # ax.set_title(area)
     return ax
 
 
@@ -173,13 +213,16 @@ def plot_comparison(
     axes=None,
     areas=None,
     median="_50",
-    upper="_97_5",
-    lower="_2_5",
-    share_ax=True,
+    upper1="_75",
+    lower1="_25",
+    upper2="_97_5",
+    lower2="_2_5",
+    scatter_actual=False,
+    sharex=True,
+    sharey=True,
     fix_scale=False,
+    estimate_colors=["tab:orange", "tab:green", "tab:red", "tab:purple", "tab:brown"],
 ):
-    estimate_colors = ["tab:orange", "tab:green", "tab:red", "tab:purple", "tab:brown"]
-
     if areas == None:
         areas = actual_results.area.unique().tolist()
 
@@ -191,9 +234,9 @@ def plot_comparison(
         fig, axes = plt.subplots(
             H,
             W,
-            sharex=share_ax,
-            sharey=share_ax,
-            figsize=(2.5 * W, 2.5 * H),
+            sharex=sharex,
+            sharey=sharey,
+            figsize=(2.5 * W, 1.5 * H),
             squeeze=False,
         )
 
@@ -204,7 +247,8 @@ def plot_comparison(
     if not isinstance(estimates_results, dict) and estimates_results is not None:
         estimates_results = {"Estimate": estimates_results}
 
-    max_y = 0.0
+    # max_y = {i: 0.0 for i in range(W)}
+    max_y = defaultdict(float)
 
     handles = []
     labels = []
@@ -213,9 +257,16 @@ def plot_comparison(
         for j, (area, ax) in enumerate(zip(areas, row)):
 
             if actual_results is not None:
-                plot_actual_area(actual_results, area, par_name=par_name, ax=ax)
-                max_y = max(
-                    max_y, actual_results[actual_results.area == area][par_name].max()
+                plot_actual_area(
+                    actual_results,
+                    area,
+                    scatter=scatter_actual,
+                    par_name=par_name,
+                    ax=ax,
+                )
+                max_y[j] = max(
+                    max_y[j],
+                    actual_results[actual_results.area == area][par_name].max(),
                 )
 
             infr_handles, ax = plot_estimate_area(
@@ -224,8 +275,10 @@ def plot_comparison(
                 par_name=par_name,
                 ax=ax,
                 median=median,
-                upper=upper,
-                lower=lower,
+                upper1=upper1,
+                lower1=lower1,
+                upper2=upper2,
+                lower2=lower2,
                 color=estimate_colors[i],
                 provenance="inferred",
             )
@@ -235,13 +288,16 @@ def plot_comparison(
                 par_name=par_name,
                 ax=ax,
                 median=median,
-                upper=upper,
-                lower=lower,
+                upper1=upper1,
+                lower1=lower1,
+                upper2=upper2,
+                lower2=lower2,
                 color=estimate_colors[i],
                 provenance="projected",
             )
-            max_y = max(
-                max_y,
+            ax.xaxis.set_major_locator(mdates.MonthLocator(interval=2))
+            max_y[j] = max(
+                max_y[j],
                 estimate[estimate.area == area][par_name + median].max(),
             )
 
@@ -253,10 +309,11 @@ def plot_comparison(
                 handles.append(proj_handles)
                 labels += [f"{name} infered", f"{name} projected"]
 
-    for ax in all_axes:
-        ax.tick_params(axis="x", rotation=45)
-        if fix_scale:
-            ax.set_ylim([0, 1.3 * max_y])
+    for i, row in enumerate(axes):
+        for j, ax in enumerate(row):
+            ax.tick_params(axis="x", rotation=45)
+            if fix_scale:
+                ax.set_ylim([0, 1.3 * max_y[j]])
 
     titles = ["Actual", *list(estimates_results.keys())]
     # lines = axes[0].get_lines()
@@ -281,7 +338,7 @@ def plot_comparison(
     #     print(h)
 
     plt.legend(
-        [(h[0][0], h[1]) for h in handles],
+        [(h[0][0], h[1], h[2]) for h in handles],
         labels,
         loc="upper left",
         bbox_to_anchor=(1.0, 0.95),
@@ -329,39 +386,91 @@ if __name__ == "__main__":
 
     os.chdir("/data/ziz/not-backed-up/mhutchin/Rmap-dev/Rmap")
     # from simulation.plot_comparisons import *
-    # %%
-    actuals = load_parameter(
-        "Rt", "simulation/latent_epidemic/tehtropolis/sample_rss_paper_1"
-    )
+    sample_folder = "simulation/latent_epidemic/tehtropolis/sample_rss_paper_1/"
+    results_folder = "simulation_fits/sample_rss_paper_1_longer/"
+    actuals = load_parameter("Rt", sample_folder)
     estimates = {
-        "SingleArea": load_all("simulation_fits/sample_rss_paper_1/singlearea"),
-        "TwoStage": load_all("simulation_fits/sample_rss_paper_1/twostage"),
-        "Regional": load_all("simulation_fits/sample_rss_paper_1/regional"),
-        "EpiNow2": load_all("simulation_fits/sample_rss_paper_1/epinow2"),
-        "EpiEstim": load_all("simulation_fits/sample_rss_paper_1/epiestim"),
+        "EpiMap (single area)": load_all(results_folder + "singlearea"),
+        # "TwoStage": load_all(results_folder + "twostage"),
+        "EpiMap (spatial: 0.05, time: 200)": load_all(results_folder + "regional"),
+        "EpiMap (spatial: 0.5, time: 200)": load_all(
+            "simulation_fits/sample_rss_paper_1_longer_space/" + "regional"
+        ),
+        "EpiNow2": load_all(results_folder + "epinow2"),
+        "EpiEstim": load_all(results_folder + "epiestim"),
     }
 
-    cases = load_cases("simulation/latent_epidemic/tehtropolis/sample_rss_paper_1")
+    cases = load_cases(sample_folder)
 
     title = "All"
 
+    mapping = {
+        "Tehtropolis": "Oxford",
+        "Brynshire": "Cherwell",
+        "Bobbingdon": "West Oxfordshire",
+        "Hutchintown": "South Oxfordshire",
+        "Shehland": "Buckinghamshire",
+    }
+
+    cases.replace({"area": mapping}, inplace=True)
+    actuals.replace({"area": mapping}, inplace=True)
+    for estimate in estimates.values():
+        estimate.replace({"area": mapping}, inplace=True)
+
+    sweep_colors = list(matplotlib.cm.get_cmap("plasma", 9).colors)[-8:-1]
+
+    estimate_colors = [
+        "black",
+        sweep_colors[2],
+        sweep_colors[5],
+        "forestgreen",
+        "crimson",
+    ]
     # %%
-    fig, axes = plot_comparison(actuals, estimates, title=title, par_name="Rt")
+    fig, axes = plot_comparison(
+        actuals,
+        estimates,
+        par_name="Rt",
+        estimate_colors=estimate_colors,
+    )
     for ax in chain.from_iterable(axes):
-        ax.set_ylim([0, 2])
+        ax.set_ylim([0, 3])
+        ax.yaxis.get_major_locator().set_params(integer=True)
+        # ax.xaxis.set_major_locator(MaxNLocator(integer=True))
+
+    # plt.locator_params(nbins=10)
+
     fig.savefig(
         f'figures/Rt_{title.lower().replace(" ", "_")}.pdf', bbox_inches="tight"
     )
     fig, axes = plot_comparison(
         cases,
         estimates,
-        title=title,
         par_name="C",
-        lower="_25",
-        upper="_975",
-        share_ax=False,
+        lower1="_25",
+        upper1="_75",
+        lower2="_025",
+        upper2="_975",
+        sharey=False,
         fix_scale=True,
+        scatter_actual=True,
+        estimate_colors=estimate_colors,
     )
+    # for ax in chain.from_iterable(axes):
+    # ax.set_yscale("log")
     fig.savefig(
         f'figures/cases_{title.lower().replace(" ", "_")}.pdf', bbox_inches="tight"
     )
+    # fig, axes = plot_comparison(
+    #     cases,
+    #     estimates,
+    #     par_name="B",
+    #     lower="_25",
+    #     upper="_975",
+    #     sharey=False,
+    #     fix_scale=True,
+    # )
+    # fig.savefig(
+    #     f'figures/cases_no_weekly_{title.lower().replace(" ", "_")}.pdf',
+    #     bbox_inches="tight",
+    # )
